37496d64a655a64c07aff8a74c983d52a1aef3a5
Usman Ali Qureshi <usmanaliquraishi@gmail.com>
2017-05-09 13:07:33 +0500

    Added plugin information

---
+++ randomize-password.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Plugin Name: Randomize Password
+* Description: A good plugin to schedule your new password after a set of days.
+* Plugin URI: https://github.com/usmanaliqureshi/randomize-password
+* Author: Usman Ali Qureshi
+* Author URI: https://www.usmanaliqureshi.com
+* Contributors: usmanaliqureshi
+* Version: 1.0.0
+* License: GPL2
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+<?php
