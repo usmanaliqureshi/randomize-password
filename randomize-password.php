ff98d98f4cf0b7302ad68362df425d2ca8e8d6e5
Usman Ali Qureshi <usman@inspirythemes.com>
2017-05-12 21:19:49 +0500

    Refactorization and Methods
    Refactorized the entire code and added activation and deactivation methods.

--- randomize-password.php
+++ randomize-password.php
@@ -1,14 +1,14 @@
 <?php
 /**
-* Plugin Name: Randomize Password
-* Description: A good plugin to schedule your new password after a set of days.
-* Plugin URI: https://github.com/usmanaliqureshi/randomize-password
-* Author: Usman Ali Qureshi
-* Author URI: https://www.usmanaliqureshi.com
-* Contributors: usmanaliqureshi
-* Version: 1.0.0
-* License: GPL2
-*/
+ * Plugin Name: Randomize Password
+ * Description: A good plugin to schedule your new password after a set of days.
+ * Plugin URI: https://github.com/usmanaliqureshi/randomize-password
+ * Author: Usman Ali Qureshi
+ * Author URI: https://www.usmanaliqureshi.com
+ * Contributors: usmanaliqureshi
+ * Version: 1.0.0
+ * License: GPL2
+ */
 
 /*
  *
@@ -16,8 +16,8 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 /*
@@ -26,48 +26,119 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  */
 
-if ( ! class_exists( Random_Password ) ) {
+if (!class_exists(Random_Password)) {
 
-	class Random_Password {
+    class Random_Password
+    {
 
-		public function rp_activation() {
+        public function __construct()
+        {
 
+            $this->rp_initialize();
 
-
-		}
-
-		public function rp_deactivation() {
-
-
-
-		}
-
-		public function rp_schedule_password() {
+        }
 
 
+        /*
+         *
+         * Initialization - Method
+         *
+         */
 
-		}
+        public function rp_initialize()
+        {
 
-		public function rp_generate_password() {
+            register_activation_hook(__FILE__, array($this, 'rp_activation'));
+
+            register_deactivation_hook(__FILE__, array($this, 'rp_deactivation'));
+
+            add_action('rp_add_schedule', array($this, 'rp_schedule_password'));
+
+        }
+
+        /*
+         *
+         * Plugin Activation - Method
+         *
+         */
+
+        public function rp_activation()
+        {
+
+            if (!wp_next_scheduled('rp_schedule_password')) {
+
+                wp_schedule_event(time(), 'daily', 'rp_add_schedule');
+
+            }
+
+        }
+
+        /*
+         *
+         * Plugin Deactivation - Method
+         *
+         */
+
+        public function rp_deactivation()
+        {
+
+            wp_clear_scheduled_hook('rp_add_schedule');
+
+        }
+
+        /*
+         *
+         * Scheduling Method
+         *
+         */
+
+        public function rp_schedule_password()
+        {
 
 
+        }
 
-		}
+        /*
+         *
+         * Strong Random Password Generator - Method
+         *
+         */
 
-		public function rp_notify_users() {
-
-
-
-		}
-
-		public function rp_change_passwords() {
-
+        public function rp_generate_password()
+        {
 
 
-		}
+        }
 
-	}
+        /*
+         *
+         * Notification to the User with new generated password - Method
+         *
+         */
+
+        public function rp_notify_users()
+        {
+
+
+        }
+
+        /*
+         *
+         * Update the user's password in WordPress - Method
+         *
+         */
+
+        public function rp_change_passwords()
+        {
+
+
+        }
+
+    }
 
 }
+
+$rp = new Random_Password();
+$rp;
 
 ?>
